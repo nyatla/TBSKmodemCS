4e8e480f109f2dafe0f0b9724e162d9ff17fdf87
nyatla <nyatla39@gmail.com>
2023-02-10 16:32:19 +0900

    rmsオーバーフローの修正 TbskTone.CreateCostom追加 v0.3.5pre

--- src/tbskmodem/tbskmodem/TbskTone.cs
+++ src/tbskmodem/tbskmodem/TbskTone.cs
@@ -54,6 +54,11 @@ namespace jp.nyatla.tbaskmodem
             return new MSeqTone(new MSequence(bits, tap), base_tone);
         }
 
+        public static TraitTone CreateCostom(IEnumerable<double> patt)
+        {
+            return new TraitTone(patt);
+        }
+
     }
 
 
--- windows/TBSKmodem/getstarted/step5_microphone/Program.cs
+++ windows/TBSKmodem/getstarted/step5_microphone/Program.cs
@@ -15,7 +15,8 @@ using (var na = new NAudioInputIterator(framerate: framerate, bits_par_sample:16
         for (; ; )
         {
             Thread.Sleep(30);
-            var v = (int)Math.Max(0, (Math.Log(na.GetRms()) + 5) * 5);
+            var rms = na.GetRms();
+            var v = (int)Math.Max(0, ((rms==0?0:Math.Log(rms)) + 5) * 5);
             Console.Write("\r" + new String('#', v) + new string(' ', (50 - v)));
         }
 
